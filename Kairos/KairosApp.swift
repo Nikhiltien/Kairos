a372c3ee6940d33ba65287891b1eda0018413e39
nikhiltien <nikhil17tn@gmail.com>
2024-02-25 16:30:10 -0500

    Initial Commit

---
+++ Kairos/KairosApp.swift
@@ -0,0 +1,32 @@
+//
+//  KairosApp.swift
+//  Kairos
+//
+//  Created by Nikhil Tien on 2/25/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KairosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
