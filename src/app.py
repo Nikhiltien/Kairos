5600067877274cbe3163e3bc8b0ab300470e79a3
nikhiltien <nikhil17tn@gmail.com>
2024-02-29 02:14:29 -0500

    django

---
+++ src/app.py
@@ -0,0 +1,8 @@
+def application(environ, start_response):
+    """Simplest possible application object"""
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
